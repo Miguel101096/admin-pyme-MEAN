36ed812c881290bca693e40a699dceeabf4fdfb9
Miguel Reina <miguel.reina@tipti.com.ec>
2021-03-21 09:53:03 -0500

    UPDATE user interface

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { FooterComponent } from './footer/footer.component';
+import { TablaCrudComponent } from './tabla-crud/tabla-crud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenubarComponent,
+    FooterComponent,
+    TablaCrudComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/tabla-crud/tabla-crud.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-crud',
+  templateUrl: './tabla-crud.component.html',
+  styleUrls: ['./tabla-crud.component.css']
+})
+export class TablaCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  search():void{
+
+  }
+}
